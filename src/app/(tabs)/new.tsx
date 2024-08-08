5db8baee4eaad3c7355260835b8e4c5ca4ac5c41
jonathan essis <jwe5279@psu.edu>
2024-08-07 21:19:42 -0400

    Image Picker with one text input

--- src/app/(tabs)/new.tsx
+++ src/app/(tabs)/new.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   Image,
   Pressable,
@@ -7,19 +7,45 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
+
+import * as ImagePicker from "expo-image-picker";
+
 export default function New() {
   const [caption, setCaption] = useState("");
+  const [image, setImage] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (!image) {
+      pickImage();
+    }
+  }, [image]);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
 
   return (
     <View className="p-3 items-center flex-1">
       {/*Image picker*/}
-      <Image
-        source={{
-          uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg",
-        }}
-        className="w-52 aspect-[3/4] rounded-lg"
-      />
-      <Text onPress={() => {}} className="text-blue-500 font-semibold m-5">
+      {image ? (
+        <Image
+          source={{ uri: image }}
+          className="w-52 aspect-[3/4] rounded-lg"
+        />
+      ) : (
+        <View className="w-52 aspect-[3/4] rounded-lg bg-slate-300" />
+      )}
+      <Text onPress={pickImage} className="text-blue-500 font-semibold m-5">
         Change
       </Text>
       {/*Input for caption*/}
