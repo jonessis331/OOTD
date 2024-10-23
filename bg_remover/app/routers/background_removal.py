74499f3689d2f32e6a881162a4fedbeb382cc801
jonathan essis <jwe5279@psu.edu>
2024-10-23 09:58:56 -0400

    removebackgorund works now locally, but I need to retrieve in closet

--- bg_remover/app/routers/background_removal.py
+++ bg_remover/app/routers/background_removal.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, HTTPException
-from fastapi.responses import Response
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from app.services.background_removal_service import remove_background_service
 import logging
+import base64
 
 router = APIRouter()
 logger = logging.getLogger(__name__)
@@ -16,7 +17,8 @@ def remove_background_endpoint(request: ImageURLRequest):
         output_io = remove_background_service(request.image_url)
         print('type of ', type(output_io))
         output_io.seek(0)
-        return Response(content=output_io.read(), media_type="image/png")
+        base64_img = base64.b64encode(output_io.read()).decode('utf-8')
+        return JSONResponse(content={"image_data": base64_img})
     except Exception as e:
         logger.exception("Error in remove_background_endpoint")
         raise HTTPException(status_code=500, detail=str(e))
